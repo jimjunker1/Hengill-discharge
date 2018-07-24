9a12c938d4e2326547566bfe5d1de2d8a74e1163
jimjunker1 <james.junker@msu.montana.edu>
2018-07-24 17:59:10 +0000

    finalized working branch

--- analysis-scripts/Q_BOM.R
+++ analysis-scripts/Q_BOM.R
@@ -8,8 +8,9 @@ Q_BOM <- subset(Q_allhr, Pd >= as.POSIXct('2011-07-31') & Pd <= as.POSIXct('2012
 temp_BOM = subset(temp_allhr, Pd >= as.POSIXct('2011-07-31') & Pd <= as.POSIXct('2012-08-15'));temp_BOM[,c(5:6)] = NULL
 tforce_BOM = subset(tforce_allhr, Pd >= as.POSIXct('2011-07-31') & Pd <= as.POSIXct('2012-08-15'))
 RBS_BOM = subset(RBS_allhr, Pd >= as.POSIXct('2011-07-31') & Pd <= as.POSIXct('2012-08-15'))
+depthe_BOM = subset(depth_allhr, Pd >= as.POSIXct('2011-07-31') & Pd <= as.POSIXct('2012-08-15'))
 
-dfs = list(Q_BOM, temp_BOM, tforce_BOM, RBS_BOM)
+dfs = list(Q_BOM, temp_BOM, tforce_BOM, RBS_BOM, depthe_BOM)
 
 dfs = lapply(dfs, setNames, nm = c("Pd","st1", "st5", "st6", "st8", "st9","st11L", "st11U",
                              "st13","st14","st17","hver"))
@@ -26,6 +27,8 @@ tforce_BOM.l = data.frame(dfs_l[3])
 colnames(tforce_BOM.l) = c("Pd", "Stream", "tforce")
 RBS_BOM.l = data.frame(dfs_l[4])
 colnames(RBS_BOM.l) = c("Pd", "Stream", "RBS")
+depthe_BOM.l = data.frame(dfs_l[5])
+colnames(depthe_BOM.l) = c("Pd", "Stream", "depth")
 
 st_temps$Stream = factor(st_temps$Stream, levels = levels(Q_BOM.l$Stream))
 rm("dfs","dfs_l");gc()
@@ -53,11 +56,16 @@ RBS_BOM.sum = RBS_BOM.l %>%
 sed_BOM.sum = sediment %>%
   group_by(Stream) %>%
   summarize(substrate = median(Size, na.rm = T))
+
+depth_BOM.sum = depthe_BOM.l %>%
+  group_by(Stream) %>%
+  summarize(depth = mean(depth, na.rm = T))
 #sed_add = data.frame(Stream = c("st8", "hver"), substrate = c(33.8,11.6))
 #sed_BOM.sum = rbind(sed_BOM.sum,sed_add)
 
 st_temps_pt1 = Reduce(function(...) merge(..., all = T), list(Q_BOM.sum, temp_BOM.sum, 
-                                                tforce_BOM.sum, RBS_BOM.sum, sed_BOM.sum))
+                                                tforce_BOM.sum, RBS_BOM.sum, sed_BOM.sum,
+                                                depth_BOM.sum))
 
 st_temps_pt2 = st_temps[which(st_temps$Date == "Jul"),c(1,4,12:13,17:18)]
 
